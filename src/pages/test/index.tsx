3a3156a3b335686d8550525babe35108e515b807
chanakaudithasandaruwan <121252932+webshealth@users.noreply.github.com>
2023-01-18 17:18:01 +0530

    Do small change

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -4,8 +4,6 @@ import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 
-// const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   interface Item {
     name: string;
