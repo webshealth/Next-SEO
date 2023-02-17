ba51f71a5eb4d7511174539e464a75b8913ad8ac
chanakaudithasandaruwan <121252932+webshealth@users.noreply.github.com>
2023-02-17 16:04:52 +0530

    Add GTM

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,28 @@
-import { useEffect } from "react";
-import { AppProps } from "next/app";
-import { useRouter } from "next/router";
-import * as gtag from "../../lib/gtag";
+import { useEffect } from 'react';
+import { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import * as gtag from '../../lib/gtag';
+import TagManager, { TagManagerArgs } from 'react-gtm-module';
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const gtmId = 'GTM-KKTXBRJ';
+
+  const tagManagerArgs: TagManagerArgs = {
+    gtmId,
+  };
   const router = useRouter();
+
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs);
+  }, []);
 
   useEffect(() => {
     const handleRouteChange = (url: URL) => {
       gtag.pageview(url);
     };
-    router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on('routeChangeComplete', handleRouteChange);
     return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
+      router.events.off('routeChangeComplete', handleRouteChange);
     };
   }, [router.events]);
 
