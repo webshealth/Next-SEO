3df5dfd907ccb925e6998559ea88d409a4da74c7
chanakaudithasandaruwan <121252932+webshealth@users.noreply.github.com>
2023-02-19 22:36:14 +0530

    Remove GTAG from _app.js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,54 +1,55 @@
 import { useEffect } from 'react';
 import { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
-import * as gtag from '../../lib/gtag';
+// import * as gtag from '../../lib/gtag';
 import TagManager, { TagManagerArgs } from 'react-gtm-module';
 import Layout from '../../components/layout';
-import Head from 'next/head';
+// import Head from 'next/head';
 
 const App = ({ Component, pageProps }: AppProps) => {
-  // const gtmId = 'GTM-KKTXBRJ';
+  const gtmId = 'GTM-KKTXBRJ';
 
-  // const tagManagerArgs: TagManagerArgs = {
-  //   gtmId,
-  // };
+  const tagManagerArgs: TagManagerArgs = {
+    gtmId,
+  };
   const router = useRouter();
 
-  // useEffect(() => {
-  //   TagManager.initialize(tagManagerArgs);
-  // }, []);
-
   useEffect(() => {
-    const handleRouteChange = (url: URL) => {
-      gtag.pageview(url);
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+
+  // useEffect(() => {
+  //   const handleRouteChange = (url: URL) => {
+  //     gtag.pageview(url);
+  //   };
+  //   router.events.on('routeChangeComplete', handleRouteChange);
+  //   return () => {
+  //     router.events.off('routeChangeComplete', handleRouteChange);
+  //   };
+  // }, [router.events]);
+
+  // <Head>
+  //       {/* Global Site Tag (gtag.js) - Google Analytics */}
+  //       <script
+  //         async
+  //         src={`https://www.googletagmanager.com/gtag/js?id=G-1GZ3X218E`}
+  //       ></script>
+  //       <script
+  //         dangerouslySetInnerHTML={{
+  //           __html: `
+  //               window.dataLayer = window.dataLayer || [];
+  //               function gtag(){dataLayer.push(arguments);}
+  //               gtag('js', new Date());
+  //               gtag('config', 'G-1GZ3X218E', {
+  //                 page_path: window.location.pathname,
+  //               });
+  //             `,
+  //         }}
+  //       />
+  //     </Head>
 
   return (
     <>
-      <Head>
-        {/* Global Site Tag (gtag.js) - Google Analytics */}
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=G-1GZ3X218E`}
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-1GZ3X218E', {
-                  page_path: window.location.pathname,
-                });
-              `,
-          }}
-        />
-      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
