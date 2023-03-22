6d8460e3f7535ed27e6ebb6ec2726d041ef3f08c
chanakaudithasandaruwan <121252932+webshealth@users.noreply.github.com>
2023-03-22 22:21:11 +0530

    test for deploy

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,10 +1,11 @@
-import { useEffect } from "react";
-import { Experiment, Variant } from "react-optimize";
-import styles from "./styles.module.css";
+import { useEffect } from 'react';
+import { Experiment, Variant } from 'react-optimize';
+import styles from './styles.module.css';
 
 function Test() {
   return (
     <div>
+      <h2>Test</h2>
       <Experiment id="lsAf1p90TZOk2bL9aAm3eA">
         <Variant id="0">Original</Variant>
         <Variant id="1">Variant 1</Variant>
