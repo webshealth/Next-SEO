917ba93825d12f6b72ebc813b46e4a8a61f065c4
chanakaudithasandaruwan <121252932+webshealth@users.noreply.github.com>
2023-06-30 16:23:22 +0530

    Add custom event to about us page

--- pages/ab/[id].tsx
+++ pages/ab/[id].tsx
@@ -1,37 +1,33 @@
-import { useEffect } from "react";
-import Cookies from "js-cookie";
-import { Text, Button } from "@vercel/examples-ui";
-import Layout from "@components/layout";
-import { getCurrentExperiment } from "@lib/optimize";
-import { useGa } from "@lib/useGa";
-import { COOKIE_NAME } from "@lib/constants";
+import { useEffect } from 'react';
+import Cookies from 'js-cookie';
+import { Text, Button } from '@vercel/examples-ui';
+import Layout from '@components/layout';
+import { getCurrentExperiment } from '@lib/optimize';
+import { useGa } from '@lib/useGa';
+import { COOKIE_NAME } from '@lib/constants';
 
 export default function Marketing({ experiment, variant }) {
   const ga = useGa();
   const sendEvent = () => {
-    const event = {
-      hitType: "event",
-      eventCategory: "AB Testing",
-      eventAction: "Clicked button",
-      eventLabel: "AB Testing About button",
-    };
-    ga("send", event);
-    console.log("sent event:", event);
+    ga('event', 'sample_name', {
+      app_name: 'myAppName',
+      screen_name: 'Home',
+    });
+
+    console.log('sent event:', event);
   };
 
   useEffect(() => {
-    const cookie = Cookies.get(COOKIE_NAME);
-    console.log(cookie);
-    console.log(ga);
-    if (ga && cookie) {
-      ga("event", "google", { value: cookie });
-    }
-    ga("send", "pageview");
-  }, [ga]);
+    console.log('Running 1');
+    ga('event', 'sample_name', {
+      experiment_name: experiment.name,
+      variant: variant,
+    });
+  }, []);
 
   //   const experiment = getCurrentExperiment();
-  console.log(variant, "experiment.heading");
-  console.log(ga, "ga");
+  console.log(variant, 'experiment.heading');
+  console.log(ga, 'ga');
   return (
     <>
       <Text variant="h2" className="mb-6">
@@ -53,15 +49,15 @@ export default function Marketing({ experiment, variant }) {
 }
 
 export async function getServerSideProps({ params }) {
-  console.log(params.id, "params");
+  console.log(params.id, 'params');
   const experiment = getCurrentExperiment();
 
-  const [, variantId] = params.id.split(".");
+  const [, variantId] = params.id.split('.');
 
   // Here you could fetch any data related only to the variant
   return {
     props: {
-      message: "nothing",
+      message: 'nothing',
       //   Only send the experiment data required by the page
       experiment: { name: experiment.name },
       variant: experiment.variants.find((v) => String(v.id) === variantId),
--- pages/ab/about.tsx
+++ pages/ab/about.tsx
@@ -1,18 +1,60 @@
-import { Text } from "@vercel/examples-ui";
-import Layout from "@components/layout";
+import { Text } from '@vercel/examples-ui';
+import Layout from '@components/layout';
+import { useEffect } from 'react';
+import { useGa } from '@lib/useGa';
+import Cookies from 'js-cookie';
+import { COOKIE_NAME } from '@lib/constants';
+import { getCurrentExperiment } from '@lib/optimize';
 
-export default function Marketing() {
+export default function Marketing({ experiment, variant }) {
+  const ga = useGa();
+  const sendEvent = () => {
+    const event = {
+      hitType: 'event',
+      eventCategory: 'AB Testing',
+      eventAction: 'Clicked button',
+      eventLabel: 'AB Testing About button',
+    };
+    ga('send', event);
+    console.log('sent event:', event);
+  };
+
+  useEffect(() => {
+    console.log('Running 0');
+    ga('event', 'ab_testing', {
+      experiment_name: experiment.name,
+      variant: variant,
+    });
+  }, []);
+
   return (
     <>
       <Text variant="h2" className="mb-6">
         About page
       </Text>
       <Text className="text-lg mb-4">
-        You&apos;re currently on <b>/marketing</b>
+        You are currently on <b>/about</b>
       </Text>
-      <Text>This is the original marketing page</Text>
+      <Text>This is the original About page</Text>
     </>
   );
+}
+
+export async function getServerSideProps(params) {
+  console.log(params, 'params');
+  const experiment = getCurrentExperiment();
+
+  // const [, variantId] = params.id.split('.');
+
+  // Here you could fetch any data related only to the variant
+  return {
+    props: {
+      message: 'nothing',
+      //   Only send the experiment data required by the page
+      experiment: { name: experiment.name },
+      variant: 'A',
+    },
+  };
 }
 
 Marketing.Layout = Layout;
