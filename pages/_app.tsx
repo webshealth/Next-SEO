74636066e72f3321a5e95a7507b5f84038a31266
webshealth <webshealthmail@gmail.com>
2023-07-03 10:56:21 +0530

    sample event added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -110,6 +110,7 @@ const App = ({ Component, pageProps, router }: AppProps) => {
               'ad_storage': 'granted',
               'analytics_storage': 'granted'
             });
+            gtag('event','appJsEvent')
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
