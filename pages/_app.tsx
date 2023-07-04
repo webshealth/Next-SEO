4c6de3eefd7106b993b7ddd8c5be2a7c94cbf7a2
webshealth <webshealthmail@gmail.com>
2023-07-04 10:59:37 +0530

    Remove GTM

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -66,39 +66,24 @@ const App = ({ Component, pageProps, router }: AppProps) => {
     };
   }, [router.events]);
 
-  <Head>
-    {/* Global Site Tag (gtag.js) - Google Analytics */}
-    <script
-      async
-      src={`https://www.googletagmanager.com/gtag/js?id=G-1GZ3X218E`}
-    ></script>
-  </Head>;
-
   return (
     <>
-      <Head>
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=G-1GZ3X218EX`}
-        ></script>
-      </Head>
       <Script
-        id="gtag"
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-1GZ3X218EX`}
+      />
+      <Script
+        id="gtag-init"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
-   gtag("event", "sample_event", {
-      experiment_name: "0",
-      variant: "1",
-    });
-        
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                      })(window,document,'script','dataLayer','GTM-KKTXBRJ');`,
+            gtag('js', new Date());
+            gtag('config', 'G-1GZ3X218EX', {
+              page_path: window.location.pathname,
+            });
+          `,
         }}
       />
       {/* <Head>
